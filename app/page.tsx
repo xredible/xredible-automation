7208a89ecaee75553161ad2f56c30b5a19de70d8
Xredible AI Automation <xredible@gmail.com>
2026-05-24 22:01:07 +0530

    Added Tawk live chat

--- app/page.tsx
+++ app/page.tsx
@@ -251,6 +251,25 @@ export default function XredibleAutomationWebsite() {
           </form>
         </div>
       </section>
+
+      {/* Tawk.to Live Chat */}
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a1324055568261c36f51b35/1jpdc9dsq';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
+
     </div>
   );
 }
